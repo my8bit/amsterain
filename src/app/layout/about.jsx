81e9e3db127076760177bfd1433707b707b35b56
my8bit <mail@my8bit.name>
2017-08-19 14:13:49 +0200

    info + icons

--- src/app/layout/about.jsx
+++ src/app/layout/about.jsx
@@ -1,16 +1,12 @@
 import React, {Component} from 'react';
-import {ColorPickerCmp} from '../components/color-picker.jsx';
+// import {ColorPickerCmp} from '../components/color-picker.jsx';
 
 export class AboutCmp extends Component {
   render() {
     return (
       <section id="about" className="about site-wrap">
         <div className="menu-background dark">Informatie</div>
-        <div className="description">Just another pomodoro timer that was inspired by Pomodoro Technique®.</div>
-        <div className="description">
-          <h4>Please select background color:</h4>
-          <ColorPickerCmp/>
-        </div>
+        <div className="description">Icons from sources Icon8 was processed at <a href="http://www.favicomatic.com/">favicomatic</a> Data from buineradar; </div>
       </section>
     );
   }
