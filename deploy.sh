402fa36fd5be9cf2437330898a99c0e581334740
Ihor Pavlenko <ihorp@backbase.com>
2017-11-30 11:41:15 +0100

    fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 DEPLOY_OUTPUT="$(netlify deploy -e stage -t $NETLIFY_TOKEN)"
+echo $DEPLOY_OUTPUT
 URL=$(grep -iIohE 'http?://[^[:space:]]+' <<< $DEPLOY_OUTPUT)
-URL=${URL/http/https}
+#URL=${URL/http/https}
 echo $URL
 node node_modules/lighthouse-ci/runlighthouse.js $URL
