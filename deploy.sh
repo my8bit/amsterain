688531e534e9183c65332ed8cc4cb85ac0970bb7
Ihor Pavlenko <my8bit@users.noreply.github.com>
2017-12-06 16:54:21 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 DEPLOY_OUTPUT="$(node_modules/netlify-cli/bin/cli.js deploy -t $NETLIFY_TOKEN -d)"
 URL=$(grep -iIohE 'https://[^[:space:]]+' <<< $DEPLOY_OUTPUT)
-echo "\033[1mSite is deployed:\033[0m"
+echo "Site is deployed:"
 echo $URL
 node node_modules/lighthouse-ci/runlighthouse.js $URL
