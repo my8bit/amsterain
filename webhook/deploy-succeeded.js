720f1dc20469bdc3c39b0cef52d05d79a50da849
my8bit <mail@my8bit.name>
2018-02-24 16:26:06 +0100

    Add lighthouse check as Netlify function

---
+++ webhook/deploy-succeeded.js
@@ -0,0 +1,39 @@
+const request = require('request');
+const {
+  LIGHTHOUSE_API_KEY,
+  DEPLOY_PRIME_URL,
+  REVIEW_ID,
+  COMMIT_REF,
+
+  REPO_OWNER,
+  REPO_NAME
+} = process.env;
+
+exports.handler = (event, context, callback) => {
+  callback(null, {
+    statusCode: 200,
+    body: ''
+  });
+  request({
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'x-api-key': LIGHTHOUSE_API_KEY
+    },
+    json: true,
+    url: 'https://lighthouse-ci.appspot.com/run_on_chrome',
+    body: {
+      testUrl: DEPLOY_PRIME_URL,
+      output: 'json',
+      addComment: true,
+      repo: {
+        owner: REPO_OWNER,
+        name: REPO_NAME
+      },
+      pr: {
+        number: REVIEW_ID,
+        sha: COMMIT_REF
+      }
+    }
+  });
+};
