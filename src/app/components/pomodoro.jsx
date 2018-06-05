a2eaf12be55aff9afa1e845c7c56205ab8facebc
Ihor Pavlenko <mail@my8bit.name>
2018-06-05 08:11:13 +0200

    add pointer

--- src/app/components/pomodoro.jsx
+++ src/app/components/pomodoro.jsx
@@ -2,8 +2,8 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 import {loadWeerAction} from '../libs/firebase.auth';
-import {VictoryVoronoiContainer, Line, VictoryCursorContainer, VictoryLabel, VictoryArea, VictoryAxis, VictoryChart, VictoryTheme} from 'victory';
-console.log(Line, VictoryCursorContainer, VictoryVoronoiContainer);  // eslint-disable-line no-console
+import {VictoryTooltip, Flyout, VictoryVoronoiContainer, Line, VictoryCursorContainer, VictoryLabel, VictoryArea, VictoryAxis, VictoryChart, VictoryTheme} from 'victory';
+console.log(Line, Flyout, VictoryTooltip, VictoryCursorContainer, VictoryVoronoiContainer);  // eslint-disable-line no-console
 // TODO: add theme
 
 class TimerWidget extends Component {
@@ -22,56 +22,39 @@ class TimerWidget extends Component {
           domain={{y: [0, 60]}}
           scale={{x: 'time'}}
           containerComponent={
-            <VictoryCursorContainer
-              cursorLabel={data => {
-                return `${data.x.toLocaleTimeString([], {
+            <VictoryVoronoiContainer
+              voronoiDimension="x"
+              labels={d => {
+                const time = d.time.toLocaleTimeString([], {
                   hour12: false,
                   hour: '2-digit',
                   minute: '2-digit'
-                })}\n0.3mm/h`;
+                });
+                const precep = Math.round(d.precep); // TODO
+                const rain = precep ? `☔ ${precep}mm/h ${time}` : '';
+                return rain;// `${rain}\n${time}`;
+                // return console.log(d) || // eslint-disable-line no-console
+                // `${Math.round(d.precep)}`;
               }}
-              onCursorChange={() => {
-                // console.log(value); // eslint-disable-line no-console
-                // return `${value}\n0.3mm/h`;
-                // const min = value.getMinutes() < 10 ? `0${value.getMinutes()}` : value.getMinutes();
-                // const time = `${value.getHours()}:${min}`;
-                // dispatch({
-                //   type: 'CHANGE_TIP',
-                //   value: 0,
-                //   time
-                // });
-              }}
-              cursorLabelComponent={
-                <VictoryLabel
+              labelComponent={<VictoryTooltip
+                labelComponent={<VictoryLabel
+                  y={25}
                   style={{
-                    fill: 'white' // TODO theme
+                    fill: 'white',
+                    fontSize: 14,
+                    fontFamily: '"Helvetica Neue", "Roboto", sans-serif'
                   }}
-                />
-              }
-              cursorComponent={
-                <Line
-                  style={{
-                    stroke: 'white',
-                    strokeWidth: 2,
-                    tickLabels: {
-                      stroke: 'white',
-                      fill: 'white' // TODO theme
-                    }
-                  }}
-                />
-              }
-              cursorDimension="x"
-              style={{
-                label: {
-                  stroke: 'white',
-                  fill: 'white' // TODO theme
-                },
-                data: {
-                  strokeWidth: 1,
-                  stroke: 'white',
-                  fill: 'white'
-                }
-              }}
+                />}
+                flyoutComponent={<Flyout
+                  y={300}
+                  height="1"
+                  cornerRadius="0"
+                  width="1"
+                  pointerLength="255"
+                  pointerWidth="1"
+                />}
+                flyoutStyle={{fill: 'red', stroke: 'red'}}
+              />}
             />
           }
           >
@@ -132,7 +115,7 @@ class TimerWidget extends Component {
                 fill: 'white' // TODO theme
               },
               grid: {
-                strokeWidth: 0
+                // strokeWidth: 0
               }
             }}
             />
@@ -157,6 +140,20 @@ class TimerWidget extends Component {
 
 /*
 TODO Component for VictoryChart
+            <VictoryVoronoiContainer
+              voronoiDimension="x"
+              labels={d => {
+                return console.log(d) || // eslint-disable-line no-console
+                `${Math.round(d.precep)}`;
+              }}
+              labelComponent={
+                <VictoryTooltip
+                  labelComponent={<VictoryLabel dy={20}/>}
+                  flyoutComponent={<Flyout/>}
+                  flyoutStyle={{fill: 'white', stroke: 'white'}}
+                />
+              }
+            />
 
             <VictoryVoronoiContainer
               labelComponent={
@@ -170,7 +167,7 @@ TODO Component for VictoryChart
               labels={d => `${d.precep} lll}`}
            />
 
-<VictoryCursorContainer
+            <VictoryCursorContainer
               cursorLabel={data => {
                 return `${data.x.toLocaleTimeString([], {
                   hour12: false,
